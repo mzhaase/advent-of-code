be701fdaa996861a75324c63170b3821d9fc5c62
mzhaase <mattis@mattis-haase.de>
2024-12-10 16:34:29 +0100

    various small optimizations

--- 2024/10/solve_optimized.py
+++ 2024/10/solve_optimized.py
@@ -4,16 +4,15 @@
 # idea 1: make a directed graph, whereby the edges always go from smaller to
 # bigger node, with weight of a - b, find all 0s, and use Djikstras to calculate
 # shortest paths that are of lenght 9
+from collections import defaultdict
 from time import time as time
 
 import networkx as nx
 
-start_time = time()
-G = nx.Graph()
-
-map    = {}
-zeroes = []
-nines  = []
+start_time      = time()
+G               = nx.Graph()
+map             = {}
+coords_by_value = defaultdict(list)
 
 print(f'Init time: {time() - start_time}')
 temp_time = time()
@@ -22,11 +21,9 @@ temp_time = time()
 with open('./input', 'r') as f:
     for y, line in enumerate(f):
         for x, node in enumerate(line.strip()):
-            map[(x, y)] = int(node)
-            if int(node) == 0:
-                zeroes.append((x, y))
-            elif int(node) == 9:
-                nines.append((x, y))
+            value       = int(node)
+            map[(x, y)] = value
+            coords_by_value[value].append((x, y))
 
 print(f'Ingest time: {time() - temp_time}')
 temp_time = time()
@@ -41,8 +38,7 @@ def add_neighbors_to_graph(x, y):
     for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
         new_x, new_y = x + dx, y + dy
         if (new_x, new_y) in map:
-            node_diff = map[(new_x, new_y)] - map[(x, y)]
-            if node_diff == 1:
+            if map[(new_x, new_y)] - map[(x, y)] == 1:
                 G.add_edge((x, y), (new_x, new_y))
 
 # runtime: 5 ms
@@ -55,15 +51,14 @@ temp_time = time()
 # runtime: 71 ms
 answer_part_one = 0
 answer_part_two = 0
-for zero in zeroes:
+for zero in coords_by_value[0]:
     nines_visited = {}
-    paths = nx.all_simple_paths(G, source=zero, target=nines, cutoff=10)
+    paths = nx.all_simple_paths(G, source=zero, target=coords_by_value[9], cutoff=10)
     try:
         for path in paths:
-            if len(path) == 10:
-                if not path[9] in nines_visited:
-                    nines_visited[path[9]] = True
-                    answer_part_one += 1
+            if not path[9] in nines_visited:
+                nines_visited[path[9]] = True
+                answer_part_one += 1
             answer_part_two += 1
     except nx.NetworkXNoPath:
         continue
